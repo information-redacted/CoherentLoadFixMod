517cd502ad2dc83ebf68fbd324435ce66d16f6ac
George Tsatsis <george@ratelimited.me>
2022-08-24 08:16:39 +0300

    chore: license code under GPLv3

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,4 +1,24 @@
-﻿using MelonLoader;
+﻿/**
+    ReSharper disable once InvalidXmlDocComment
+    
+    CoherentLoadFix - A ChilloutVR mod to fix Coherent Gameface's Native Imports
+    Copyright (C) 2022  [information redacted]
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+using MelonLoader;
 
 [assembly: MelonInfo(typeof(CoherentLoadFix.CoherentLoadFix), "CoherentLoadFix", "1.0.0", "[information redacted]")]
 [assembly: MelonGame("Alpha Blend Interactive", "ChilloutVR")]
