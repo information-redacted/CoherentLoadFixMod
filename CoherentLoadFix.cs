fb853348fc64e3d4c2e1c89bb6f2b7cd70f79db5
George Tsatsis <george@ratelimited.me>
2022-08-24 08:06:07 +0300

    Project init; Works.

---
+++ CoherentLoadFix.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using MelonLoader;
+
+namespace CoherentLoadFix;
+
+public class CoherentLoadFix : MelonMod
+{
+    private static MelonPreferences_Category CoherentLoadFixPreferences;
+    private static MelonPreferences_Entry<bool> CoherentLoadFixEnabled;
+    private static MelonPreferences_Entry<bool> CoherentLoadFixHTTPServerEnabled;
+    private static MelonPreferences_Entry<bool> CoherentLoadFixMediaDecodersEnabled;
+    private static string DllDirectory = $"{Directory.GetCurrentDirectory()}\\ChilloutVR_Data\\Plugins\\x86_64\\";
+    
+    #region Win32 API Deps
+    [DllImport("kernel32", SetLastError = true)]
+    private static extern IntPtr LoadLibraryW([MarshalAs(UnmanagedType.LPWStr)]string lpFileName);
+    #endregion
+    
+    
+    public override void OnApplicationStart()
+    {
+        CoherentLoadFixPreferences = MelonPreferences.CreateCategory("Coherent Load Fix");
+        CoherentLoadFixEnabled = CoherentLoadFixPreferences.CreateEntry<bool>("Enabled", true, "Enable CoherentLoadFix");
+        CoherentLoadFixHTTPServerEnabled = CoherentLoadFixPreferences.CreateEntry<bool>("HTTPServerEnabled", true, "Load the HttpServer plugin");
+        CoherentLoadFixMediaDecodersEnabled = CoherentLoadFixPreferences.CreateEntry<bool>("MediaDecodersEnabled", true, "Load the MediaDecoders plugin");
+
+        if (!CoherentLoadFixEnabled.Value) return;
+        if (CoherentLoadFixHTTPServerEnabled.Value) LoadLibraryW($"{DllDirectory}HttpServer.WindowsDesktop.dll");
+        if (CoherentLoadFixMediaDecodersEnabled.Value) LoadLibraryW($"{DllDirectory}MediaDecoders.WindowsDesktop.dll");
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,4 @@
+﻿using MelonLoader;
+
+[assembly: MelonInfo(typeof(CoherentLoadFix.CoherentLoadFix), "CoherentLoadFix", "1.0.0", "[information redacted]")]
+[assembly: MelonGame("Alpha Blend Interactive", "ChilloutVR")]
